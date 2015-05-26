0c0e4d8a249bbc2cbd0dc3066f4b7bd507295149
Sebastian YEPES F <syepes@gmail.com>
2015-05-27 00:26:37 +0200

    Add InfluxDB support

--- src/main/groovy/MetricClient.groovy
+++ src/main/groovy/MetricClient.groovy
@@ -12,14 +12,20 @@ import groovy.transform.WithWriteLock
 import java.util.concurrent.locks.ReentrantReadWriteLock
 import java.util.concurrent.locks.ReadWriteLock
 
+import java.util.zip.GZIPOutputStream
+import java.net.URL
+import java.net.HttpURLConnection
+
 import net.razorvine.pickle.*
 
 
 @Slf4j
 class MetricClient {
-  String graphite_host, protocol, prefix
-  int graphite_port
-  final int socketTimeOut = 10000
+  String server_host, server_auth, protocol, prefix
+  int server_port
+  final int socketTimeOut = 10000 // ms
+  final int http_readTimeout = 30000 // ms
+  final int http_connectTimeout = 5000 // ms
   final int maxTries = 2
 
   private final ReadWriteLock mBufferLock = new ReentrantReadWriteLock()
@@ -28,9 +34,10 @@ class MetricClient {
   private final LinkedList mBuffer = []
   private final LinkedList mBufferPickle = []
 
-  MetricClient(String graphite_host = 'localhost', int graphite_port = 2003, String protocol = 'tcp', String prefix = null) {
-    this.graphite_host = graphite_host
-    this.graphite_port = graphite_port
+  MetricClient(String server_host = 'localhost', int server_port = 2003, String protocol = 'tcp', String prefix = null, String server_auth = '') {
+    this.server_host = server_host
+    this.server_port = server_port
+    this.server_auth = server_auth
     this.protocol = protocol?.toLowerCase()
     this.prefix = prefix
   }
@@ -104,6 +111,7 @@ class MetricClient {
    * Send metrics using Text format to the Graphite server
    *
    * @param metrics List of metric strings
+   * @param useBuffer Boolean Toggle to enable Buffering of failures
    */
   void send2Graphite(ArrayList metrics, Boolean useBuffer=true) {
     if (!metrics) { return }
@@ -112,11 +120,11 @@ class MetricClient {
     int sentCount = 0
     def socket
 
-    log.debug "Sending Metrics to Graphite (${graphite_host}:${graphite_port}) using '${protocol}' (useBuffer: ${useBuffer})"
+    log.debug "Sending Metrics to Graphite (${server_host}:${server_port}) using '${protocol}' (useBuffer: ${useBuffer})"
 
     try {
       if (protocol == 'tcp') {
-        socket = new Socket(graphite_host, graphite_port)
+        socket = new Socket(server_host, server_port)
         socket.setSoTimeout(socketTimeOut)
       } else {
         socket = new DatagramSocket()
@@ -153,8 +161,8 @@ class MetricClient {
             writer.flush()
           } else {
             byte[] bytes = msg.getBytes()
-            InetAddress addr = InetAddress.getByName(graphite_host)
-            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, addr, graphite_port)
+            InetAddress addr = InetAddress.getByName(server_host)
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, addr, server_port)
             socket?.send(packet)
           }
           sentCount++
@@ -187,8 +195,8 @@ class MetricClient {
           writer.flush()
         } else {
           byte[] bytes = msg.getBytes()
-          InetAddress addr = InetAddress.getByName(graphite_host)
-          DatagramPacket packet = new DatagramPacket(bytes, bytes.length, addr, graphite_port)
+          InetAddress addr = InetAddress.getByName(server_host)
+          DatagramPacket packet = new DatagramPacket(bytes, bytes.length, addr, server_port)
           socket?.send(packet)
         }
         sentCount++
@@ -217,6 +225,7 @@ class MetricClient {
    * Send metrics using Pickle format to the Graphite server
    *
    * @param metrics List of metric strings
+   * @param useBuffer Boolean Toggle to enable Buffering of failures
    */
   void send2GraphitePickle(ArrayList metrics, Boolean useBuffer=true) {
     if (!metrics) { return }
@@ -227,10 +236,10 @@ class MetricClient {
     ArrayList picklePkgs = []
     ArrayList pickleBufferPkgs = []
 
-    log.debug "Sending Metrics to Graphite (${graphite_host}:${graphite_port}) using 'tcp' (useBuffer: ${useBuffer})"
+    log.debug "Sending Metrics to Graphite (${server_host}:${server_port}) using 'tcp' (useBuffer: ${useBuffer})"
 
     try {
-      socket = new Socket(graphite_host, graphite_port)
+      socket = new Socket(server_host, server_port)
       socket.setSoTimeout(socketTimeOut)
 
     } catch (Exception e) {
@@ -359,9 +368,13 @@ class MetricClient {
   }
 
 
-  /*
-   * Generate a list of Pickler packages that not reache Carbon maxLength 1048576:Bytes = 1:MB
+  /**
+   * Generate a list of Pickler packages that does not reach the Carbon maxLength 1048576:Bytes = 1:MB
    *
+   * @param metrics List of metric strings
+   * @param maxLength Long maximum package length
+   *
+   * @return ArrayList of the generated Pickler packages
    */
   ArrayList generatePicklerPkgs(ArrayList metrics, long maxLength=972800) {
     if (!metrics) { return [] }
@@ -397,5 +410,237 @@ class MetricClient {
     Date timeEnd = new Date()
     log.info "Created ${pkgs?.size()} Pickler Packages for ${metrics?.size()} Metrics in ${TimeCategory.minus(timeEnd, timeStart)}"
     return pkgs
+  }
+
+
+
+  /**
+   * Check InfluxDB Status
+   *
+   * @return Boolean indicating if the server is available
+   */
+  Boolean pingInfluxDB() {
+    Boolean status = false
+    String basicAuth,response
+
+    try {
+      if (server_auth?.contains(':')) {
+        basicAuth = "${server_auth?.split(':')?.getAt(0)}:${server_auth?.split(':')?.getAt(1)}".getBytes().encodeBase64().toString()
+      }
+
+      URL url = new URL("http://${server_host}:${server_port}/messaging/ping")
+      HttpURLConnection con = url.openConnection()
+      con.setRequestProperty('Accept', 'application/json; charset=UTF-8')
+      con.setRequestProperty('User-Agent', 'MetricClient')
+      if (basicAuth) {
+        con.setRequestProperty('Authorization', "Basic ${basicAuth}")
+      }
+      con.setRequestMethod('GET')
+      con.useCaches = false
+      con.readTimeout = http_readTimeout // ms
+      con.connectTimeout = http_connectTimeout // ms
+      con.allowUserInteraction = false
+
+      con.connect()
+      if (con.responseCode == HttpURLConnection.HTTP_OK) {
+        response = con?.responseMessage?.trim()
+        if (response =~ /(?i).*OK.*/){ status = true }
+      } else { status = false }
+
+    } catch(Exception e) {
+      StackTraceUtils.deepSanitize(e)
+      log.error "Ping InfluxDB: ${status ? 'OK' : 'Failed'} (${response}) ${con?.getHeaderField('X-Influxdb-Version')?: ''} : ${e?.message}"
+      log.debug "Ping InfluxDB: ${getStackTrace(e)}"
+    }
+
+    if (status) {
+      log.info "Ping InfluxDB: ${status ? 'OK' : 'Failed'}"
+    } else {
+      log.error "Ping InfluxDB: ${status ? 'OK' : 'Failed'} (${response}) ${con?.getHeaderField('X-Influxdb-Version')?: ''}"
+    }
+    return status
+  }
+
+
+  /**
+   * Write InfluxDB data using the HTTP API
+   *
+   * @param json String with the plain InfluxDB JSON Post
+   * @param compression Boolean Toggle to enable HTTP compression
+   */
+  private void writeInfluxDB(String json, Boolean compression=true) {
+    if (!json) { return }
+    String basicAuth
+
+    try {
+      if (server_auth?.contains(':')) {
+        basicAuth = "${server_auth?.split(':')?.getAt(0)}:${server_auth?.split(':')?.getAt(1)}".getBytes().encodeBase64().toString()
+      }
+
+      URL url = new URL("http://${server_host}:${server_port}/write")
+      HttpURLConnection con = url.openConnection()
+      if (basicAuth) {
+        con.setRequestProperty('Authorization', "Basic ${basicAuth}")
+      }
+      con.setRequestProperty('Content-type', 'application/json; charset=UTF-8')
+      con.setRequestProperty('Accept', 'application/json; charset=UTF-8')
+      con.setRequestProperty('User-Agent', 'MetricClient')
+      con.setRequestMethod('POST')
+      con.doOutput = true
+      con.useCaches = false
+      con.readTimeout = http_readTimeout // ms
+      con.connectTimeout = http_connectTimeout // ms
+      con.allowUserInteraction = false
+
+      if (compression) {
+        con.setRequestProperty('Content-Encoding', 'gzip')
+        con.getOutputStream().write( string2gzip(json) )
+      } else {
+        OutputStreamWriter osw = new OutputStreamWriter(con.getOutputStream(), 'UTF-8')
+        osw.write(json)
+        osw.close()
+      }
+
+      Integer responseCode = con?.getResponseCode()
+      if (responseCode == HttpURLConnection.HTTP_NO_CONTENT) {
+        log.info "Sent InfluxDB Metric ${compression ? 'Compressed ' : ''}(${responseCode})"
+
+        // Ignore Response content
+        con.getInputStream().close()
+      } else {
+        BufferedReader br = new BufferedReader(new InputStreamReader(con?.getErrorStream()))
+        log.error "Sending InfluxDB Metric to '${url}' (${responseCode}:'${con.getResponseMessage()}') - ${br?.readLine()}"
+        br?.close()
+        throw new Exception("Sending InfluxDB Metric to '${url}'")
+      }
+
+    } catch(Exception e) {
+      StackTraceUtils.deepSanitize(e)
+      log.error "Sending InfluxDB Metric: ${e?.message}"
+      log.debug "Sending InfluxDB Metric: ${getStackTrace(e)}"
+      throw new Exception("Sending InfluxDB Metric")
+    }
+  }
+
+
+  /**
+   * Compress String using GZIP
+   *
+   * @param s String that will be compressed
+   *
+   * @return byte[] Compressed string
+   */
+  private byte[] string2gzip(String s) throws Exception {
+    ByteArrayOutputStream bos = new ByteArrayOutputStream()
+    GZIPOutputStream gzip = new GZIPOutputStream(bos)
+    OutputStreamWriter osw = new OutputStreamWriter(gzip, 'UTF-8')
+    osw.write(s)
+    osw.close()
+    return bos.toByteArray()
+  }
+
+
+  /**
+   * Send metrics using the InfluxDB HTTP API
+   *
+   * @param metrics List of metric (json) strings
+   * @param useBuffer Boolean Toggle to enable Buffering of failures
+   */
+  void send2InfluxDB(ArrayList metrics, Boolean useBuffer=true) {
+    if (!metrics) { return }
+
+    Date timeStart = new Date()
+    int sentCount = 0
+
+    log.debug "Sending Metrics to InfluxDB (${server_host}:${server_port}) using '${protocol}' (useBuffer: ${useBuffer})"
+
+    try {
+      // Buffer if InfluxDB is not available
+      if (!pingInfluxDB()) {
+        if (useBuffer) {
+          addBufferItems(metrics)
+          log.warn "Added ${metrics?.size()} Metrics in the Buffer (mBuffer: ${getBufferSize()})"
+        }
+
+        return
+      }
+
+    } catch (Exception e) {
+      StackTraceUtils.deepSanitize(e)
+      log.error "InfluxDB Status: ${e?.message}"
+      log.debug "InfluxDB Status: ${getStackTrace(e)}"
+
+      if (useBuffer) {
+        addBufferItems(metrics)
+        log.warn "Added ${metrics?.size()} Metrics in the Buffer (mBuffer: ${getBufferSize()})"
+      }
+
+      return
+    }
+
+    // Send buffered metrics first
+    if (getBufferSize() && useBuffer) {
+      log.info "Sending ${getBufferSize()} Buffered Metrics"
+      int sendTries = 0
+
+      // Send metrics
+      while (getBufferSize() > 0 && sendTries <= maxTries) {
+        String msg = pollBufferItem()
+        log.trace "Metric: ${msg} (mBuffer: ${getBufferSize()})"
+
+        try {
+          if (protocol == 'http-compression') {
+            writeInfluxDB(msg)
+          } else if (protocol == 'http') {
+            writeInfluxDB(msg, false)
+          } else {
+            log.error "Unknown InfluxDB protocol: ${protocol}"
+          }
+          sentCount++
+
+        } catch(Exception e) {
+          StackTraceUtils.deepSanitize(e)
+          log.warn "Sending Buffered Metric: ${e?.message}"
+          log.debug "Sending Buffered Metric: ${getStackTrace(e)}"
+
+          sendTries++
+          addBufferItem(msg)
+          log.warn "Buffered Metric added to the Buffer (mBuffer: ${getBufferSize()})"
+        }
+
+        if (sendTries >= maxTries) {
+          log.error "Sending Buffered Metric reached its maximum retries (${sendTries} >= ${maxTries})"
+        }
+      }
+    }
+
+    // Send metrics
+    log.info "Sending ${metrics?.size()} Metrics"
+    metrics.each { String msg ->
+
+      try {
+        if (protocol == 'http-compression') {
+          writeInfluxDB(msg)
+        } else if (protocol == 'http') {
+          writeInfluxDB(msg, false)
+        } else {
+          log.error "Unknown InfluxDB protocol: ${protocol}"
+        }
+        sentCount++
+
+      } catch(Exception e) {
+        StackTraceUtils.deepSanitize(e)
+        log.warn "Sending Metric: ${e?.message}"
+        log.debug "Sending Metric: ${getStackTrace(e)}"
+
+        if (useBuffer) {
+          addBufferItem(msg)
+          log.warn "Metric added to the Buffer (mBuffer: ${getBufferSize()})"
+        }
+      }
+    }
+
+    Date timeEnd = new Date()
+    log.info "Finished sending ${sentCount} Metrics (mBuffer: ${getBufferSize()}) to InfluxDB in ${TimeCategory.minus(timeEnd, timeStart)}"
   }
 }
